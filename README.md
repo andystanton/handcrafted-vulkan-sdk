53286125454a2001bad5edc2499568c8f84392dc
Andy Stanton <andystanton@gmail.com>
2019-12-11 22:52:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Handcrafted Vulkan SDK
+
+A script that assembles something that looks like v1.1.126 of the [LunarG Vulkan SDK](http://lunarg.com/vulkan-sdk/) for macOS by checking out and building the individual components.
+
+## Background
+
+As part of macOS Catalina, Apple require binaries to be notarized. This meant that the executables and libraries included in the LunarG Vulkan SDK up to at least v1.1.126 [are not directly usable](https://vulkan.lunarg.com/issue/view/5d9b73d786da81451cc37e7b) in macOS Catalina.
+
+There is a workaround: it's possible to right click on a non-notarized excutable or library and ⌥-click on Open. This presents you with a dialog that asks if you want to allow this file to be opened. You have to repeat this a few times for each of the libraries and executables you might want to use. For me this was the glslc executable, and the Vulkan loader, MoltenVK, Khronos validation layer and LunarG API dump validation layer libraries. Yes, you have to 'open' libraries to use this workaround.
+
+As of v1.1.126, LunarG do not publicly provide the process by which they put together the SDK. This script aims to produce a similar output to the LunarG Vulkan SDK, partly to address the notarization issue, but also to allow custom builds of the SDK.
+
+## What's in it?
+
+The following repositories are built in this order:
+
+* [KhronosGroup/SPIRV-Headers](https://github.com/KhronosGroup/SPIRV-Headers)
+* [KhronosGroup/SPIRV-Tools](https://github.com/KhronosGroup/SPIRV-Tools)
+* [KhronosGroup/SPIRV-Cross](https://github.com/KhronosGroup/SPIRV-Cross)
+* [KhronosGroup/glslang](https://github.com/KhronosGroup/glslang)
+* [google/shaderc](https://github.com/google/shaderc)
+* [KhronosGroup/Vulkan-Headers](https://github.com/KhronosGroup/Vulkan-Headers)
+* [KhronosGroup/Vulkan-Loader](https://github.com/KhronosGroup/Vulkan-Loader)
+* [KhronosGroup/Vulkan-ValidationLayers](https://github.com/KhronosGroup/Vulkan-ValidationLayers)
+* [KhronosGroup/MoltenVK](https://github.com/KhronosGroup/MoltenVK)
+* [KhronosGroup/Vulkan-Tools](https://github.com/KhronosGroup/Vulkan-Tools)
+* [LunarG/VulkanTools](https://github.com/LunarG/VulkanTools)
+
+## Requirements
+
+* CMake
+* make
+* Python 3
+* Xcode
+
+### Optional
+
+* Qt5 for building vkconfig.app
+
+## Build
+
+```sh
+git clone https://github.com/andystanton/handcrafted-vulkan-sdk.git
+cd handcrafted-vulkan-sdk
+./build-sdk.sh
+```
+
+This will create a directory called `vulkansdk-macos-1.1.126.handcrafted` containing the SDK. On a 2015 Macbook Pro with 4 cores and 16GB RAM this takes about 1 hour. A rebuild takes about a minute.
+
+## Installation
+
+I have intentionally not provided a way to install to system paths like `/usr/local` because this script is already juggling several chainsaws.
+
+Instead you can move the built SDK to a known path and set environment variables as described in the [LunarG Vulkan SDK docs](https://vulkan.lunarg.com/doc/sdk/latest/mac/getting_started.html).
+
+For example I move it to `$HOME/.vulkan/vulkansdk-macos-1.1.126.handcrafted` and create a symlink `$HOME/.vulkan/current` that points to it. My environment variables are then:
+
+```sh
+export VULKAN_SDK="$HOME/.vulkan/current/macOS"
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$VULKAN_SDK/lib"
+export VK_ICD_FILENAMES="$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json"
+export VK_LAYER_PATH="$VULKAN_SDK/etc/vulkan/explicit_layer.d"
+export PATH="$PATH:$VULKAN_SDK/bin"
+```
+
+I have tested this with my personal CMake projects and they work as expected with the builtin `find_package(vulkan)`.
+
+## Future
+
+The future of this script is based around solving the following problems:
+
+### This script exists
+
+This script doesn't need to exist. For it to be obsolete:
+1. Apple could endorse Vulkan and drop the notarization requirement for it, even if they don't support it. This is unlikely to happen.
+2. LunarG could notarize the binaries in the SDK. They have said they will do this! But it would also be great if...
+3. LunarG could make their SDK creation process open source. Being able to run custom builds of the SDK would be very cool!
+
+### Each repository has its own way of managing its dependencies
+
+The dependency management of these projects is varied. Most have similar-but-slightly-different approaches to version management. Some of the projects perform their own builds of other projects that are part of the SDK. This is exacerbated by C/C++ projects generally being awful to manage dependencies for.
+
+I don't have a solution for this. It would be great if Khronos and LunarG could come up with a uniform way of doing this. I don't know if that would include doing something other than CMake based builds because C/C++ package managers terrify me.
+
+### The script is only for macOS
+
+My primary use case is to address the notarization issue introduced with macOS Catalina, but supporting custom SDK builds would be useful across all platforms. This script was put together by assembling the build instructions for each of its components for macOS, so extending it to support Linux and adding a Windows version is a case of doing the same for those platforms.
+
+## License
+
+This script is MIT licensed. The software it builds is subject to whatever licenses used by that software.
+
+## Disclaimer
+
+I do not work with low-level graphics APIs or C/C++ as a profession - I am only a hobbyist with graphics programming. Therefore do not trust what I say on the subject and please do the appropriate research yourself.
---
+++ build-sdk.sh
@@ -0,0 +1,338 @@
+#!/usr/bin/env bash
+
+set -e
+
+# =============================================================================
+# build constants
+# =============================================================================
+
+pushd $(dirname ${0}) >/dev/null
+script_name=$(basename ${0})
+script_path=$(pwd -P)
+popd >/dev/null
+
+sdk_version=1.1.126
+
+source_path=${script_path}/source
+build_path=${script_path}/build
+base_output_path=${script_path}/vulkansdk-macos-${sdk_version}.handcrafted
+output_path=${base_output_path}/macOS
+
+repos=(
+    KhronosGroup:SPIRV-Headers:38cafab379e5d16137cb97a485b9385191039b92
+    KhronosGroup:SPIRV-Tools:9b3cc3e05337358d0bd9fec1b7a51e3cbf55312b
+    KhronosGroup:SPIRV-Cross:2019-11-01
+    KhronosGroup:glslang:4b5159ea8170fa34e29f13448fddebf88e0a722a
+    google:shaderc:known-good
+    KhronosGroup:Vulkan-Headers:v${sdk_version}
+    KhronosGroup:Vulkan-Loader:v${sdk_version}
+    KhronosGroup:Vulkan-ValidationLayers:v${sdk_version}
+    KhronosGroup:MoltenVK:v1.0.38
+    KhronosGroup:Vulkan-Tools:v${sdk_version}
+    LunarG:VulkanTools:sdk-${sdk_version}
+)
+
+# =============================================================================
+# build utility functions
+# =============================================================================
+
+function get_revision() {
+    target_org=$1
+    target_repo=$2
+    repo_regex='^(.+):(.+):(.+)$'
+
+    for repo in ${repos[@]}; do
+        gh_org=$(sed -E 's/'${repo_regex}'/\1/' <<<$repo)
+        gh_repo=$(sed -E 's/'${repo_regex}'/\2/' <<<$repo)
+        gh_version=$(sed -E 's/'${repo_regex}'/\3/' <<<$repo)
+        if [[ ${gh_org} == ${target_org} ]] && [[ ${gh_repo} == ${target_repo} ]]; then
+            echo ${gh_version}
+            return
+        fi
+    done
+    echo "unknown repository"
+}
+
+function get_source() {
+    gh_org=$1
+    gh_repo=$2
+    gh_version=$(get_revision ${gh_org} ${gh_repo})
+    gh_path=${source_path}/${gh_repo}
+
+    echo "Fetching ${gh_org}/${gh_repo}:${gh_version}"
+
+    if [ ! -d ${gh_path} ]; then
+        git clone "https://github.com/${gh_org}/${gh_repo}.git" ${gh_path} &>/dev/null
+    fi
+
+    pushd ${gh_path} >/dev/null
+
+    git fetch --all >/dev/null
+    git -c advice.detachedHead=false checkout ${gh_version} &>/dev/null
+    git pull origin ${gh_version} &>/dev/null
+
+    popd >/dev/null
+}
+
+function cmake_build() {
+    project_repo=${1}; shift
+    project_generator=${1}; shift
+    project_name=$(sed -E 's/^([^\/]+)\/.*$/\1/' <<<$project_repo)
+    project_source_path=${source_path}/${project_repo}
+    project_build_path=${build_path}/${project_name}
+
+    cmake_generator='Unknown'
+    extra_build_args=()
+    case $project_generator in
+    make)
+        cmake_generator='Unix Makefiles' 
+        extra_build_args=(-- --quiet)
+        ;;
+    ninja)
+        cmake_generator='Ninja' 
+        ;;
+    esac
+
+    echo "Executing cmake build for ${project_name}"
+
+    mkdir -p ${project_build_path}
+    pushd ${project_build_path} >/dev/null
+
+    cmake \
+        "-G${cmake_generator}" \
+        -Wno-dev \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=${output_path} \
+        "${@}" \
+        ${project_source_path}
+
+    cmake \
+        --build . \
+        --target install \
+        --parallel $(sysctl -n hw.activecpu) \
+        "${extra_build_args[@]}"
+
+    popd >/dev/null
+}
+
+function xcode_build() {
+    project_repo=${1}; shift
+    project_xcode_proj=${1}; shift
+    project_scheme=${1}; shift
+    project_name=$(sed -E 's/^([^\/]+)\/.*$/\1/' <<<$project_repo)
+    project_source_path=${source_path}/${project_repo}
+    project_build_path=${build_path}/${project_name}
+
+    echo "Executing xcode build for ${project_name}"
+
+    mkdir -p ${project_build_path}
+    pushd ${project_build_path} >/dev/null
+
+    xcodebuild \
+        -quiet \
+        -project "${project_xcode_proj}" \
+        -scheme "${project_scheme}" \
+        "${@}" \
+        build
+
+    popd >/dev/null
+}
+
+function fix_library_path() {
+    source_file=$1
+    target_file=$2
+    search_regex='(\"library_path\":[[:space:]]*\")\.\/(lib[[:alnum:]_]+\.dylib)(\")'
+    replace_regex='\1..\/..\/..\/lib\/\2\3'
+    sed -E "s/${search_regex}/${replace_regex}/" ${source_file} >${target_file}
+}
+
+# =============================================================================
+# per-project build functions
+# =============================================================================
+
+function build_spirv_headers() {
+    get_source KhronosGroup SPIRV-Headers
+    cmake_build SPIRV-Headers make
+}
+
+function build_spirv_tools() {
+    get_source KhronosGroup SPIRV-Tools
+
+    pushd ${source_path}/SPIRV-Tools >/dev/null
+    python3 ./utils/git-sync-deps >/dev/null
+    popd >/dev/null
+    
+    cmake_build SPIRV-Tools make \
+        -DSPIRV_SKIP_TESTS=on
+}
+
+function build_spirv_cross() {
+    get_source KhronosGroup SPIRV-Cross
+    cmake_build SPIRV-Cross make \
+        -DSPIRV_CROSS_EXCEPTIONS_TO_ASSERTIONS=on \
+        -DSPIRV_CROSS_ENABLE_TESTS=off \
+        -DSPIRV_CROSS_STATIC=on \
+        -DSPIRV_CROSS_SHARED=on \
+        -DSPIRV_CROSS_CLI=on
+}
+
+function build_glslang() {
+    get_source KhronosGroup glslang
+
+    pushd ${source_path}/glslang >/dev/null
+    ./update_glslang_sources.py >/dev/null 
+    popd >/dev/null
+
+    cmake_build glslang make \
+        -DENABLE_OPT=off
+}
+
+function build_shaderc() {
+    get_source google shaderc
+
+    pushd ${source_path}/shaderc >/dev/null
+    ./update_shaderc_sources.py >/dev/null
+    popd >/dev/null
+
+    cmake_build shaderc/src ninja \
+        -DSKIP_GLSLANG_INSTALL=on \
+        -DSKIP_SPIRV_TOOLS_INSTALL=on \
+        -DSHADERC_ENABLE_INSTALL=on \
+        -DINSTALL_GTEST=off
+}
+
+function build_vulkan_headers() {
+    get_source KhronosGroup Vulkan-Headers
+    cmake_build Vulkan-Headers make
+}
+
+function build_vulkan_loader() {
+    get_source KhronosGroup Vulkan-Loader
+    cmake_build Vulkan-Loader make \
+        -DVULKAN_HEADERS_INSTALL_DIR=${output_path}
+
+    rm -rf ${output_path}/Frameworks
+    mv ${output_path}/loader ${output_path}/Frameworks
+}
+
+function build_vulkan_validation_layers() {
+    get_source KhronosGroup Vulkan-ValidationLayers
+
+    pushd ${source_path}/Vulkan-ValidationLayers >/dev/null
+    ./scripts/update_deps.py --config release >/dev/null
+    popd >/dev/null
+
+    cmake_build Vulkan-ValidationLayers make \
+        -DVULKAN_HEADERS_INSTALL_DIR=${output_path} \
+        -DGLSLANG_INSTALL_DIR=${output_path} \
+        -DCMAKE_INSTALL_RPATH:PATH=${output_path}/lib \
+        -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=TRUE \
+        -C ${source_path}/${gh_repo}/helper.cmake \
+
+    mkdir -p ${output_path}/etc/vulkan/explicit_layer.d
+    fix_library_path \
+        ${build_path}/Vulkan-ValidationLayers/layers/VkLayer_khronos_validation.json \
+        ${output_path}/etc/vulkan/explicit_layer.d/VkLayer_khronos_validation.json
+
+    mkdir -p ${output_path}/lib
+    cp ${build_path}/Vulkan-ValidationLayers/layers/libVkLayer_khronos_validation.dylib ${output_path}/lib
+}
+
+function build_molten_vk() {
+    get_source KhronosGroup MoltenVK
+
+    pushd ${source_path}/MoltenVK >/dev/null
+    ./fetchDependencies \
+        --spirv-cross-root ${source_path}/SPIRV-Cross \
+        --v-headers-root ${source_path}/Vulkan-Headers \
+        >/dev/null
+    popd >/dev/null
+
+    mkdir -p ${build_path}/MoltenVK
+
+    # The trailing `|| :` is a hack because the script uses `set -e` so is needed to prevent 
+    #  the script from exiting when the `cp` command fails after the initial run because 
+    #  of existing symlinks.
+    cp -RH ${source_path}/${gh_repo}/ ${build_path}/MoltenVK/ 2>/dev/null || :
+
+    xcode_build MoltenVK MoltenVKPackaging.xcodeproj "MoltenVK Package" \
+        GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS MVK_LOGGING_ENABLED=0'
+
+    rm -rf ${base_output_path}/MoltenVK
+    cp -RH ${build_path}/MoltenVK/Package/Release/MoltenVK ${base_output_path} 2>/dev/null || :
+    cp ${build_path}/MoltenVK/Package/Latest/MoltenVK/macOS/dynamic/libMoltenVK.dylib ${output_path}/lib 2>/dev/null || :
+
+    mkdir -p ${output_path}/etc/vulkan/icd.d
+    fix_library_path \
+        ${build_path}/MoltenVK/MoltenVK/icd/MoltenVK_icd.json \
+        ${output_path}/etc/vulkan/icd.d/MoltenVK_icd.json
+}
+
+function build_khronos_vulkan_tools() {
+    get_source KhronosGroup Vulkan-Tools
+
+    cmake_build Vulkan-Tools make \
+        -DVULKAN_HEADERS_INSTALL_DIR=${output_path} \
+        -DVULKAN_LOADER_INSTALL_DIR=${output_path} \
+        -DMOLTENVK_REPO_ROOT=${build_path}/MoltenVK
+
+    rm -rf ${base_output_path}/Applications/vkcube.app \
+        ${base_output_path}/Applications/vkcubepp.app \
+        ${base_output_path}/Applications/vulkaninfo.app
+
+    mkdir -p ${base_output_path}/Applications
+    mv ${output_path}/cube/vkcube.app \
+        ${output_path}/cube/vkcubepp.app \
+        ${output_path}/vulkaninfo/vulkaninfo.app \
+        ${base_output_path}/Applications
+
+    rm -rf ${output_path}/cube \
+        ${output_path}/vulkaninfo
+}
+
+function build_lunarg_vulkan_tools() {
+    get_source LunarG VulkanTools
+
+    pushd ${source_path}/VulkanTools >/dev/null
+    ./update_external_sources.sh >/dev/null 
+    popd >/dev/null
+
+    pushd ${source_path}/VulkanTools >/dev/null
+    ./scripts/update_deps.py >/dev/null
+    popd >/dev/null
+
+    export CMAKE_PREFIX_PATH='/usr/local/opt/qt:$CMAKE_PREFIX_PATH'
+    cmake_build VulkanTools make \
+        -DVULKAN_HEADERS_INSTALL_DIR=${output_path} \
+        -DVULKAN_LOADER_INSTALL_DIR=${output_path} \
+        -DVULKAN_VALIDATIONLAYERS_INSTALL_DIR=${output_path} \
+        -C ${source_path}/VulkanTools/helper.cmake
+
+    mkdir -p ${output_path}/etc/vulkan/explicit_layer.d
+    fix_library_path \
+        ${build_path}/${gh_repo}/layersvt/VkLayer_api_dump.json \
+        ${output_path}/etc/vulkan/explicit_layer.d/VkLayer_api_dump.json
+
+    rm -rf ${base_output_path}/Applications/vkconfig.app
+    if [ -d ${output_path}/bin/vkconfig.app ]; then
+        mv ${output_path}/bin/vkconfig.app ${base_output_path}/Applications
+    fi
+}
+
+# =============================================================================
+# build process
+# =============================================================================
+
+mkdir -p ${source_path} ${build_path} ${output_path}
+
+build_spirv_headers
+build_spirv_tools
+build_spirv_cross
+build_glslang
+build_shaderc
+build_vulkan_headers
+build_vulkan_loader
+build_vulkan_validation_layers
+build_molten_vk
+build_khronos_vulkan_tools
+build_lunarg_vulkan_tools
